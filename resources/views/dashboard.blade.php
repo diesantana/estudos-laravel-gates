82a49ae6dd45bf6f7fcb0b11d2cf39d3372b7f86
Diego Santana <3c.santana@gmail.com>
2026-04-01 18:43:35 -0300

    feat: renderiza o post-component na view dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,32 +4,9 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 flex flex-col gap-8">
             @forelse ($posts as $post)
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg mb-6">
-                    {{-- post card --}}
-                    <div class="p-6 text-gray-900">
-                        {{-- header card --}}
-                        <div class="flex justify-between text-zinc-500 font-medium pb-3">
-                            {{-- autor --}}
-                            <div>
-                                <p class="">Autor: <span class="text-blue-700">{{ $post->user->name }}</span></p>
-                            </div>
-                            {{-- Data de criação --}}
-                            <div>
-                                <p>Criado em: <span> {{ $post->created_at->format('d/m/Y H:i')}}</span></p>
-                            </div>
-                        </div>
-
-                        {{-- conteúdo --}}
-                        <div class="p-4">
-                            {{-- título do post --}}
-                            <h3 class="font-bold text-2xl pb-2">{{ $post->title}}</h3>
-                            {{-- conteúdo do post --}}
-                            <p>{{ $post->body}}</p>
-                        </div>
-                    </div>
-                </div>
+                <x-post-component :post='$post'/>
             @empty
                 <p class="text-gray-900">Nennhum POST cadastrado!</p>
             @endforelse
