ad41c5fc47004988df40493ee89e3e60a86b1ddc
Diego Santana <3c.santana@gmail.com>
2026-04-01 19:45:23 -0300

    feat: Cria os gates create-post e delete-post

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +22,18 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        /**
+         * Define se o user pode criar um post
+         */
+        Gate::define('create-post', function (User $user) {
+            return $user->role === 'admin' || $user->role === 'normal_user';
+        });
+
+        /**
+         * Define se o user pode deletar um post
+         */
+        Gate::define('delete-post', function (User $user, Post $post) {
+            return $user->role === 'admin' || $user->id === $post->user_id;
+        });
     }
 }
