6638b7e57ababbecf9b131f110305055194dba7d
Diego Santana <3c.santana@gmail.com>
2026-04-01 18:43:19 -0300

    feat: Implementa o layout do post-component

---
+++ resources/views/components/post-component.blade.php
@@ -0,0 +1,24 @@
+<div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+    {{-- post card --}}
+    <div class="p-6 text-gray-900">
+        {{-- header card --}}
+        <div class="flex justify-between text-zinc-500 font-medium pb-3">
+            {{-- autor --}}
+            <div>
+                <p class="">Autor: <span class="text-blue-700">{{ $post->user->name }}</span></p>
+            </div>
+            {{-- Data de criação --}}
+            <div>
+                <p>Criado em: <span> {{ $post->created_at->format('d/m/Y H:i') }}</span></p>
+            </div>
+        </div>
+
+        {{-- conteúdo --}}
+        <div class="p-4">
+            {{-- título do post --}}
+            <h3 class="font-bold text-2xl pb-2">{{ $post->title }}</h3>
+            {{-- conteúdo do post --}}
+            <p>{{ $post->body }}</p>
+        </div>
+    </div>
+</div>
