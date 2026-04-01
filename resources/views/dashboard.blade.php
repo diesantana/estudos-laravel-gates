aeddbb2932ab93c2831fa07246c1fd3a09bc2a82
Diego Santana <3c.santana@gmail.com>
2026-04-01 19:46:03 -0300

    feat: Implementa a lógica para exibir o btn criar de acordo com o gate

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,8 +5,21 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 flex flex-col gap-8">
+            {{-- ==================================================== --}}
+            {{-- Habilita o botão de criar post de acordo com o gate --}}
+            {{-- ==================================================== --}}
+            @can('create-post')
+                <div>
+                    <a href="#" class="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition">
+                        Criar Post
+                    </a>
+                </div>
+            @endcan
+            {{-- ==================================================== --}}
+            {{-- ==================================================== --}}
+
             @forelse ($posts as $post)
-                <x-post-component :post='$post'/>
+                <x-post-component :post='$post' />
             @empty
                 <p class="text-gray-900">Nennhum POST cadastrado!</p>
             @endforelse
