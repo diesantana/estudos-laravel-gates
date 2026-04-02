19cf33273f1036aed812ee004021ead56e615ac9
Diego Santana <3c.santana@gmail.com>
2026-04-02 08:20:15 -0300

    feat: Implementa a lógica de autorização no controller

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\View\View;
 
 class MainController extends Controller
@@ -16,5 +17,28 @@ class MainController extends Controller
         // Busca os posts
         $posts = Post::with('user')->get();
         return view('dashboard', ['posts' => $posts]);
+    }
+
+    public function create()
+    {
+        // Verifica se o user tem permissão
+        if (! Gate::allows('create-post')) {
+            abort(403, 'Você não tem permissão para criar um post');
+        }
+
+        return 'Cria o post';
+    }
+
+    public function destroy(int $id)
+    {
+        // Busca o post
+        $post = Post::find($id);
+
+        // Verifica se o user tem permissão
+        if (! Gate::allows('delete-post', $post)) {
+            abort(403, 'Você não tem permissão para deletar este post');
+        }
+
+        return 'Deletar o post';
     }
 }
